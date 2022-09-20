f4224e7606a715807c8031a8f0ca0f6696b6a401
bunmi <estaroberts@yahoo.com>
2022-09-20 22:33:41 +0000

    updated

--- facts.sh
+++ facts.sh
@@ -1,4 +1,5 @@
 # demo script fot git. Facts nonetheless
+# test script fot git stash, apply and drop
 echo what God cannot do, does not exist
 echo God has the only say
 echo I am blessed
@@ -10,3 +11,4 @@ echo Jesus died so that I may live
 echo Jesus is my Savior
 echo I am a miracle
 echo I am a success story
+echo I am a child of the Most High
