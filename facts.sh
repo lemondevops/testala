33104975b1037c3ade9b62c8c51c048dd1c93a8b
bunmi <estaroberts@yahoo.com>
2022-09-20 22:21:29 +0000

    true story

---
+++ facts.sh
@@ -0,0 +1,12 @@
+# demo script fot git. Facts nonetheless
+echo what God cannot do, does not exist
+echo God has the only say
+echo I am blessed
+echo I am valuable
+echo I am favored
+echo I am protected and loved
+echo God loves me
+echo Jesus died so that I may live
+echo Jesus is my Savior
+echo I am a miracle
+echo I am a success story
